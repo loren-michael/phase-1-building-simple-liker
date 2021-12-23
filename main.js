17c9c774c0b8830cc73f81f9b8357fb4b6cdf702
loren <lorenmichaelh@gmail.com>
2021-12-23 17:09:44 -0600

    commit tree d4fa0593d5643f37d2fcabee0fe3b6598578c79b

--- main.js
+++ main.js
@@ -2,8 +2,43 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+const heart = document.querySelector(".like-glyph");
+const modal = document.getElementById("modal");
+const modalMessage = document.getElementById("modal-message");
+const heartElements = [...document.getElementsByClassName("like-glyph")];
 
+
+// My code below
+
+
+let catchError = (e) => {
+  mimicServerCall()
+  .then (() => handleResponse(e))
+  .catch (error => handleError(error))
+  }
+
+let heartEvents = heartElements.map(heartNode => {
+  return heartNode.addEventListener('click', catchError);
+})
+
+let handleError = (errorMessage) => {
+  modal.classList.remove("hidden");
+  modalMessage.innerText = errorMessage;
+  setTimeout(() => {
+    modal.classList.add("hidden")
+    modalMessage.innerText = "";
+  }, 3000);
+}
+
+let handleResponse = (event) => {
+  if (event.target.textContent === EMPTY_HEART) {
+    event.target.classList.add("activated-heart");
+    event.target.textContent = FULL_HEART
+  } else {
+    event.target.classList.remove('activated-heart');
+    event.target.textContent = EMPTY_HEART;
+  }
+}
 
 
 
